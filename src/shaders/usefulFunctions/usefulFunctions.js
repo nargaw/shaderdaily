768a6a2adb51b7fe13d24f503317197c6786154f
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-12-04 22:00:31 -0500

    shader no. 433

--- src/shaders/usefulFunctions/usefulFunctions.js
+++ src/shaders/usefulFunctions/usefulFunctions.js
@@ -52,6 +52,16 @@ float sdCircle(vec2 p, float r)
     return 1. - smoothstep(0.01, 0.03, x);
 }
 
+float sdCircleOutline(vec2 p, float r)
+{
+    p = p * 2.0 - 1.;
+    float x = length(p) - r;
+    float y = length(p) - r + 0.05;
+    float x1 = 1. - smoothstep(0.01, 0.03, x);
+    float y1 = 1. - smoothstep(0.01, 0.03, y);
+    return x1 - y1;
+}
+
 float sdSegment(vec2 p, vec2 a, vec2 b)
 {
     vec2 pa = p-a;
