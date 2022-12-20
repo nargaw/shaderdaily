2e2eaf46c4fc4dfbd54ebae0dff26b0b22afd846
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-12-20 05:29:49 -0500

    shader no. 449

---
+++ src/shaders/448/fragment.js
@@ -0,0 +1,62 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+const fragmentShader = 
+glsl`
+float label(vec2 p)
+{
+    p *= 10.;
+    p.x -= 0.25;
+    float left = sdFour(vec2(p.x + 0.3, p.y));
+    float center = sdFour(vec2(p.x -0.05, p.y));
+    float right = sdEight(vec2(p.x - 0.38, p.y));
+    return left + center + right;
+}
+
+float newfunc(vec2 p, float a)
+{
+p = Rot(p , u_time);
+p = p * 2. - 1.;
+// //p.y -= 1.25;
+// p.x += 1.;
+// p.y += 1.;
+// p*= 0.5;
+float b;
+float steps = 15.;
+
+for(float i = 1.; i<steps; i++)
+{
+    // p = Rot(p, u_time * 0.25);
+    // p = p * 2. - 1.;
+    p = p * i / (3.);
+    float k = 1. + i * (0.25 - 0.25 * cos(u_time * i * 0.025));
+    b += sdSpiral(p * i, a  * i, k + i * 0.2);
+    b += sdSegment(p * i , vec2(0.0), vec2(0.25));
+}
+return b;
+}
+
+void main()
+{
+vec2 vUv = vec2(vUv.x, vUv.y);
+vec3 color = vec3(0.);
+float numLabel = label(vUv);
+vec2 newUv = vUv;
+// vUv = vUv * 2. - 1.;
+float k = 1. + 20. * (0.5 - 0.5 * cos(u_time + 1.5));
+float spiral = sdSpiral(vUv, 1.0, k);
+// color += spiral;
+vUv = Rot(vUv, u_time);
+newUv = Rot(newUv, PI * 0.25);
+float func = newfunc(vUv, 1.0);
+float func2 = newfunc(newUv, 1.0);
+// color.g += func * 0.25;
+color.r += func * 0.25 - abs(cos(u_time * 0.5));
+color.r += func2 * 0.25 - abs(cos(u_time * 0.5));
+color.b += func * 0.25 - (sin(u_time * 0.5)  /2.) - 0.5;
+color.b += func2 * 0.25 - (sin(u_time * 0.5)  /2.) - 0.5;
+color += numLabel + numLabel + numLabel;
+gl_FragColor = vec4(color, 1.);
+}
+`
+
+export default fragmentShader
--- src/shaders/usefulFunctions/usefulFunctions.js
+++ src/shaders/usefulFunctions/usefulFunctions.js
@@ -94,11 +94,11 @@ float quadraticBezier (float x, vec2 a){
     return y;
 }
 
-float random (vec2 st) {
-    return fract(sin(dot(st.xy,
-                         vec2(12.9898,78.233)))*
-        43758.5453123);
-}
+// float random (vec2 st) {
+//     return fract(sin(dot(st.xy,
+//                          vec2(12.9898,78.233)))*
+//         43758.5453123);
+// }
 
 float plot(vec2 p, float line, float thickness)
 {
