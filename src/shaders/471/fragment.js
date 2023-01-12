4910954f8b8a77d74bd910338d46f0c20094ca71
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-01-12 04:59:56 -0500

    shader no. 471

---
+++ src/shaders/471/fragment.js
@@ -0,0 +1,48 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+const fragmentShader = glsl`
+    float label(vec2 p)
+    {
+        p *= 10.;
+        p.x -= 0.25;
+        float left = sdFour(vec2(p.x + 0.3, p.y));
+        float center = sdSeven(vec2(p.x -0.035, p.y));
+        float right = sdOne(vec2(p.x - 0.39, p.y));
+        return left + center + right;
+    }
+
+    void main()
+    {
+        vec2 vUv = vec2(vUv.x, vUv.y);
+        vec3 color = vec3(0.);
+        float shape1;
+        float shape2;
+        float shape3;
+        vec2 newvUv = vUv;
+        newvUv = Rot(newvUv, u_time * 0.5);
+        newvUv = newvUv * 4. - 2.;
+        
+        float k = 1. + 20. * (0.5 - 0.5 + (0.025 + (sin(u_time * 0.25) + .5)/015.));
+        for (int i=0; i<=10; i++)
+        {
+            // shape1 += sdSpiral(newvUv, 0.5* float(i), k);
+            // shape2 += sdSpiral(newvUv, 0.5* float(i), 20./k );
+            // shape3 += sdSpiral(newvUv, 0.5* float(i), 100./k );
+            shape1 += sdEqTriangleOutline(vec2(newvUv.x + sin(float(i) * u_time * 0.25), newvUv.y + cos(float(i) * u_time * 0.25)), 0.3 * (float(i) * 0.1));
+            // shape2 += sdRoundedBoxOutline(newvUv, vec2(0.5* float(i) + cos(u_time * 0.25), 0.5* float(i)), vec4(float(i)), 0.1);
+            // shape3 += sdRoundedBoxOutline(newvUv, vec2(0.5* float(i) + cos(u_time * 0.35), 0.5* float(i)), vec4(float(i)), 0.1);
+        }
+        
+        
+        // float shape5 = sdPolygonOutline(vec2(vUv.x, vUv.y - 0.01), 3.0 + sin(u_time), 0.5);
+        color += shape1;
+        // color *= shape2;
+        // color *= shape3;
+
+        float numLabel = label(vUv);
+        color += numLabel;
+        gl_FragColor = vec4(color, 1.);
+    }
+`
+
+export default fragmentShader
