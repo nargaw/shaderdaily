06331e2c30414b4ec12ab15110ea018b794d9e4d
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-01-24 04:51:16 -0500

    shader no. 483

---
+++ src/shaders/483/fragment.js
@@ -0,0 +1,46 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+const fragmentShader = glsl`
+    float label(vec2 p)
+    {
+        p *= 10.;
+        p.x -= 0.25;
+        float left = sdFour(vec2(p.x + 0.3, p.y));
+        float center = sdEight(vec2(p.x -0.035, p.y));
+        float right = sdThree(vec2(p.x - 0.39, p.y));
+        return left + center + right;
+    }
+
+    void main()
+    {
+        vec2 vUv = vec2(vUv.x, vUv.y);
+        vec3 color = vec3(0.);
+        float shape1;
+        float shape2;
+        float shape3;
+        float shape4;
+        vec2 uv2 = vUv;
+        
+        
+        // uv2.x += 1.;
+        // uv2.y -0.2;
+        // uv2 = Rot(uv2, PI * 0.5);
+        uv2 = uv2 *5. - 2.;
+        // uv2 = Rot(uv2, u_time * 0.1);
+        for (int i=1; i<=15; i++)
+        {
+            // uv2 = uv2 * 1.1 - 0.1;
+            uv2 = Rot(uv2, u_time * 0.05 * float(i));
+            shape1 += sdSegment(vec2(uv2.x + float(i) /10. - 0.25, uv2.y + float(i) /10. - 0.25), vec2(0.0), vec2(0.25));
+            
+        }
+        
+        color += shape1; 
+        
+        float numLabel = label(vUv);
+        color += numLabel;
+        gl_FragColor = vec4(color, 1.);
+    }
+`
+
+export default fragmentShader
