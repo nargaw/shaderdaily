1124837480c511d52095c0330264bff8c36f7137
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-05-09 12:59:16 -0400

    shader no. 564

---
+++ src/shaders/564/fragment.js
@@ -0,0 +1,278 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+const fragmentShader = glsl`
+    float label(vec2 p)
+    {
+        p *= 10.;
+        p.x -= 0.25;
+        float left = numFive(vec2(p.x + 0.35, p.y));
+        float center = numSix(vec2(p.x -0.03, p.y));
+        float right = numFour(vec2(p.x - 0.42, p.y));
+        return left + center + right ;
+    }
+
+    //https://timcoster.com/2020/03/05/raymarching-shader-pt5-colors/
+
+    const vec4 ColorRed = vec4(1,0,0,1);
+    const vec4 GroundColor = vec4(vec3(0.6), 1.);
+    float colorIntensity = 1.;
+    vec3 difColor = vec3(1.0, 1.0, 1.0); // Diffuse Color
+
+    mat2 Rotate(float a) {
+        float s=sin(a); float c=cos(a);
+        return mat2(c,-s,s,c);
+    }
+
+    ///////////////////////
+    // Boolean Operators
+    ///////////////////////
+    
+    vec4 intersectSDF(vec4 a, vec4 b) {
+        return a.w > b.w ? a : b;
+    }
+      
+    vec4 unionSDF(vec4 a, vec4 b) {
+        return a.w < b.w? a : b;
+    }
+     
+    vec4 differenceSDF(vec4 a, vec4 b) {
+        return a.w > -b.w? a : vec4(b.rgb,-b.w);
+    }
+
+    /////////////////////////////
+    // Smooth blending operators
+    /////////////////////////////
+    
+    vec4 smoothIntersectSDF(vec4 a, vec4 b, float k ) 
+    {
+        float h = clamp(0.5 - 0.5*(a.w-b.w)/k, 0., 1.);
+        vec3 c = mix(a.rgb,b.rgb,h);
+        float d = mix(a.w,b.w,h) + k*h*(1.-h);
+        
+        return vec4(c,d);
+    }
+    
+    vec4 smoothUnionSDF(vec4 a, vec4 b, float k ) 
+    {
+        float h = clamp(0.5 + 0.5*(a.w-b.w)/k, 0., 1.);
+        vec3 c = mix(a.rgb,b.rgb,h);
+        float d = mix(a.w, b.w, h) - k*h*(1.-h); 
+        
+        return vec4(c,d);
+    }
+    
+    vec4 smoothDifferenceSDF(vec4 a, vec4 b, float k) 
+    {
+        float h = clamp(0.5 - 0.5*(a.w+b.w)/k, 0., 1.);
+        vec3 c = mix(a.rgb,b.rgb,h);
+        float d = mix(a.w, -b.w, h ) + k*h*(1.-h);
+        
+        return vec4(c,d);
+    }
+/////////////////////////
+
+    #define MAX_STEPS 100
+    #define MAX_DIST 100.
+    #define SURF_DIST .01
+
+    ///////////////////////
+// Primitives
+///////////////////////
+ 
+// Sphere - exact
+    float sphereSDF( vec3 p, float s ) {
+        
+        return (length(p)-s);
+    }
+
+    float distortSphere(vec3 p)
+    {
+        float displacement = sin(5.0 * p.x) * sin(2.0 * p.y) * sin(5.0 * p.z) * 0.25;
+        float sphere_0 = sphereSDF(p, .25);
+
+        return sphere_0 + displacement;
+    }
+
+    
+    // Box - exact
+    float boxSDF( vec3 p, vec3 b ) {
+        vec3 q = abs(p) - b;
+        return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+    }
+
+    // Rounded Box - exact
+    float roundedBoxSDF( vec3 p, vec3 b, float r ) {
+        vec3 q = abs(p) - b;
+        return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
+    }
+
+    //box frame
+    float boxFrameSDF( vec3 p, vec3 b, float e )
+    {
+           p = abs(p  )-b;
+      vec3 q = abs(p+e)-e;
+      return min(min(
+          length(max(vec3(p.x,q.y,q.z),0.0))+min(max(p.x,max(q.y,q.z)),0.0),
+          length(max(vec3(q.x,p.y,q.z),0.0))+min(max(q.x,max(p.y,q.z)),0.0)),
+          length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
+    }
+
+    float dot2( in vec3 v ) { return dot(v,v); }
+    float sdBoxFrame( vec3 p, vec3 b, float e){
+        p = abs(p  )-b;
+        vec3 q = abs(p+e)-e;
+        return sqrt(min(min(dot2(max(vec3(p.x,q.y,q.z),0.0)),
+                dot2(max(vec3(q.x,p.y,q.z),0.0))),
+                dot2(max(vec3(q.x,q.y,p.z),0.0)))) 
+                +min(0.0,min(min( max(p.x,max(q.y,q.z)),
+                max(p.y,max(q.z,q.x))),
+                max(p.z,max(q.x,q.y))));
+    }
+    
+    float cappedCylinderSDF( vec3 p, float h, float r ) {
+        vec2 d = abs(vec2(length(p.xz),p.y)) - vec2(r,h);
+        return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+    }
+    
+    // Plane - exact
+    float planeSDF(vec3 p,vec4 n) {
+        // n must be normalized
+        return dot(p,n.xyz)+n.w;
+    }
+    
+    // Triangular Prism - exact
+    float triPrismSDF(vec3 p,vec2 h) {
+        const float k=sqrt(3.);
+        h.x*=.5*k;
+        p.xy/=h.x;
+        p.x=abs(p.x)-1.;
+        p.y=p.y+1./k;
+        if(p.x+k*p.y>0.)p.xy=vec2(p.x-k*p.y,-k*p.x-p.y)/2.;
+        p.x-=clamp(p.x,-2.,0.);
+        float d1=length(p.xy)*sign(-p.y)*h.x;
+        float d2=abs(p.z)-h.y;
+        return length(max(vec2(d1,d2),0.))+min(max(d1,d2),0.);
+    }
+    
+    // Rounded Cylinder - exact
+    float roundedCylinderSDF(vec3 p,float ra,float rb,float h){
+        vec2 d=vec2(length(p.xz)-2.*ra+rb,abs(p.y)-h);
+        return min(max(d.x,d.y),0.)+length(max(d,0.))-rb;
+    }
+
+    //capped torus
+    float cappedTorusSDF(vec3 p, vec2 sc, float ra, float rb ){
+        p.x = abs(p.x);
+        float k = (sc.y*p.x>sc.x*p.y) ? dot(p.xy,sc) : length(p.xy);
+        return sqrt(dot(p,p) + ra*ra - 2.0 * ra * k ) - rb;
+    }
+
+
+    vec4 GetDist(vec3 p)
+    {
+    // Sphere.
+    vec3 s0p=vec3(0.,1.75,0.75);
+    float displacement =  sin(2.0 * p.x + (u_time)/1.) * sin(2.0 * p.y + (u_time)/2.) * sin(2.0 * p.z + (u_time)/1.) * 0.37525;
+    s0p=p-s0p + displacement;
+    
+    // s0p=p-s0p - displacement;
+    // s0p += displacement;
+    // s0p.z += sin(u_time);
+    float distortedSphere = distortSphere(p);
+    vec4 s0 = vec4(vec3(ColorRed),sphereSDF(s0p, 1.));
+   
+    // Plane
+    vec4 p0 = vec4(GroundColor.rgb,planeSDF(p,vec4(0,1,0,0)));
+ 
+    vec4 scene = vec4(0), csg0 = vec4(0);
+     
+    csg0 = smoothDifferenceSDF(s0, p0, 0.125); // Intersect 
+
+    scene = smoothUnionSDF(s0, p0, 0.15);
+ 
+    return scene;
+    }
+
+    float RayMarch(vec3 ro,vec3 rd, inout vec3 dColor)
+    {
+        float dO=0.;//Distane Origin
+        for(int i=0;i<MAX_STEPS;i++)
+        {
+            if(dO>MAX_DIST)
+                break;
+    
+            vec3 p=ro+rd*dO;
+            vec4 ds=GetDist(p);// ds is Distance Scene
+    
+            if(ds.w<SURF_DIST)
+            {
+                dColor = ds.rgb;
+                break;
+            }
+            dO+=ds.w;
+            
+        }
+        return dO;
+    }
+    
+    vec3 GetNormal(vec3 p)
+    {
+        float d=GetDist(p).w;// Distance
+        vec2 e=vec2(.01,0);// Epsilon
+        
+        vec3 n=d-vec3(
+            GetDist(p-e.xyy).w,// e.xyy is the same as vec3(.01,0,0). The x of e is .01. this is called a swizzle
+            GetDist(p-e.yxy).w,
+            GetDist(p-e.yyx).w);
+            
+        return normalize(n);
+    }
+
+    vec3 GetLight(vec3 p, vec3 c)
+    {
+        // Diffuse Color
+        vec3 color = c.rgb * colorIntensity;
+    
+        // Directional light
+        vec3 lightPos=vec3(-2.,6.,-5.);// Light Position
+    
+        vec3 l=normalize(lightPos-p);// Light Vector
+        vec3 n=GetNormal(p);// Normal Vector
+        
+        float dif=dot(n,l);// Diffuse light
+        dif=clamp(dif,0.,1.);// Clamp so it doesnt go below 0
+        
+        // Shadows
+        float d=RayMarch(p+n*SURF_DIST*2.,l,difColor);
+        
+        if(d<length(lightPos-p))dif*=.1;
+        
+        return color * dif;
+    }
+
+    void main()
+    {
+        vec2 vUv = vec2(vUv.x, vUv.y);
+        vec3 color = vec3(0.);
+        
+        vec2 uv2 = vUv;
+        uv2 -= 0.5;
+
+        vec3 ro = vec3(0,3.5,-5.0); // Ray Origin/Camera position
+        vec3 rd = normalize(vec3(uv2.x,uv2.y,1)); // Ray Direction
+
+        rd.zy *= Rotate(PI*-.1); // Rotate camera down on the x-axis
+        
+        float d=RayMarch(ro,rd,difColor);// Distance
+
+        vec3 p=ro+rd*d;
+        vec3 dif=GetLight(p,difColor);// Diffuse lighting
+        color  = vec3(dif);
+
+        float numLabel = label(vUv);
+        color += numLabel;
+        gl_FragColor = vec4(color, 1.);
+    }
+`
+
+export default fragmentShader
