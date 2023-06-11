6a379168b4783ffb2314f6e7d6edce41ceebb27e
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-06-11 19:24:58 -0400

    shader no. 574

---
+++ src/shaders/574/fragment.js
@@ -0,0 +1,38 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+const fragmentShader = glsl`
+
+    #define S(a, b, t) smoothstep(a, b, t)
+    
+    float label(vec2 p)
+    {
+        p *= 10.;
+        p.x -= 0.25;
+        float left = numFive(vec2(p.x + 0.35, p.y));
+        float center = numSeven(vec2(p.x -0.03, p.y));
+        float right = numFour(vec2(p.x - 0.42, p.y));
+        return left + center + right ;
+    }
+    
+    void main()
+    {
+        vec2 vUv = vec2(vUv.x, vUv.y);
+        vec3 color = vec3(0.);
+        
+        vec2 uv2 = vUv;
+        uv2 -= 0.5;
+
+        float one = 1. - sdBox(uv2, vec2(2.5, 1./7.));
+        float two = 1. - sdBox(vec2(uv2.x, uv2.y + 1./7.), vec2(2.5, 1./7.));
+
+        color += one * vec3(1., 0., 0.);
+        color += two * vec3(1., 1., 0.);
+        
+
+        float numLabel = label(vUv);
+        color += numLabel;
+        gl_FragColor = vec4(color, 1.);
+    }
+`
+
+export default fragmentShader
