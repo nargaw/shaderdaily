1edfc0a02324f75a949862cae7268cd7832a0c72
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-07-08 09:59:51 -0400

    update routes 578 to 586

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ import { useFrame } from '@react-three/fiber'
 import Display from './Display.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { OrbitControls } from '@react-three/drei'
-import Shader585 from './shaders/585/fragment.js'
+
+import Shader578 from './shaders/578/fragment.js'
+import Shader579 from './shaders/579/fragment.js'
+import Shader580 from './shaders/580/fragment.js'
+import Shader581 from './shaders/581/fragment.js'
+import Shader582 from './shaders/582/fragment.js'
+import Shader583 from './shaders/583/fragment.js'
 import Shader584 from './shaders/584/fragment.js'
+import Shader585 from './shaders/585/fragment.js'
 import Shader586 from './shaders/586/fragment.js'
 
 
@@ -41,16 +48,22 @@ export default function App()
     //     meshRef.current.material.uniforms.u_time.value = clock.elapsedTime
     // })
 
-    const today = '584';
+    const today = 584;
     const current = today;
 
     return (
         <>
-            <OrbitControls />
+            {/* <OrbitControls /> */}
             <BrowserRouter>
                 <Routes>
                     <Route path='/'>
                         <Route index element={<Shader586 />} />
+                        <Route path='578' element={<Shader578 />}/>
+                        <Route path='579' element={<Shader579 />}/>
+                        <Route path='580' element={<Shader580 />}/>
+                        <Route path='581' element={<Shader581 />}/>
+                        <Route path='582' element={<Shader582 />}/>
+                        <Route path='583' element={<Shader583 />}/>
                         <Route path='584' element={<Shader584 />}/>
                         <Route path='585' element={<Shader585 />}/>
                         <Route path='586' element={<Shader586 />}/>
