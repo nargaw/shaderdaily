e5bb4ca854df0ebb00603cfcf77a6bed93eac1e1
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-07-20 06:55:49 -0400

    shader no. 595

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import Shader591 from './shaders/591/fragment.js'
 import Shader592 from './shaders/592/fragment.js'
 import Shader593 from './shaders/593/fragment.js'
 import Shader594 from './shaders/594/fragment.js'
+import Shader595 from './shaders/595/fragment.js'
 
 export default function App()
 {
@@ -59,7 +60,8 @@ export default function App()
         <Shader591 />,
         <Shader592 />,
         <Shader593 />,
-        <Shader594 />
+        <Shader594 />,
+        <Shader595 />
     ]
 
     return (
@@ -95,6 +97,7 @@ export default function App()
                         <Route path='592' element={<Shader592 />}/>
                         <Route path='593' element={<Shader593 />}/>
                         <Route path='594' element={<Shader594 />}/>
+                        <Route path='595' element={<Shader595 />}/>
                     {/* </Route>  */}
                 </Routes>
             </BrowserRouter>
---
+++ src/shaders/595/fragment.js
@@ -0,0 +1,148 @@
+import glsl from 'babel-plugin-glsl/macro'
+
+import { useGLTF } from '@react-three/drei'
+
+const fragmentShader = glsl`
+
+    #define S(a, b, t) smoothstep(a, b, t)
+    
+    float label(vec2 p)
+    {
+        p *= 10.;
+        p.x -= 0.25;
+        float left = numFive(vec2(p.x + 0.35, p.y));
+        float center = numNine(vec2(p.x -0.03, p.y));
+        float right = numFive(vec2(p.x - 0.42, p.y));
+        return left + center + right ;
+    }
+
+
+    vec3 skycolour1 = vec3(0.0, 0.3, 0.8);
+    vec3 skycolour2 = vec3(0.8, 0.9, 1.0);
+
+    float random (in vec2 _st) {
+        return fract(sin(dot(_st.xy,
+                             vec2(12.9898,78.233)))*
+            43758.5453123);
+    }
+
+    // Based on Morgan McGuire @morgan3d
+    // https://www.shadertoy.com/view/4dS3Wd
+    float noise (in vec2 _st) {
+        vec2 i = floor(_st);
+        vec2 f = fract(_st);
+
+        // Four corners in 2D of a tile
+        float a = random(i);
+        float b = random(i + vec2(1.0, 0.0));
+        float c = random(i + vec2(0.0, 1.0));
+        float d = random(i + vec2(1.0, 1.0));
+
+        vec2 u = f * f * (3.0 - 2.0 * f);
+
+        return mix(a, b, u.x) +
+                (c - a)* u.y * (1.0 - u.x) +
+                (d - b) * u.x * u.y;
+    }
+
+    #define NUM_OCTAVES 4
+
+    float fbm ( in vec2 _st) {
+        float v = 0.0;
+        float a = 0.55;
+        vec2 shift = vec2(100.0);
+        // Rotate to reduce axial bias
+        mat2 rot = mat2(cos(0.5), sin(0.5),
+                        -sin(0.5), cos(0.50));
+        for (int i = 0; i < NUM_OCTAVES; ++i) {
+            v += a * noise(_st);
+            _st = rot * _st * 2.0 + shift;
+            a *= 0.5;
+        }
+        return v;
+    }
+    
+    void main()
+    {
+        vec2 vUv = vec2(vUv.x, vUv.y);
+        vec3 color = vec3(0.);
+
+        vec2 uv2 = vUv;
+        uv2 -= 0.52;
+        uv2 *= 3.9;
+        uv2.x += u_time*0.1;
+        uv2.y += u_time * 0.1;
+        
+        vec2 q = vec2(0.);
+        q.y = fbm( uv2 + 0.2*u_time);
+        q.x = fbm( uv2 + u_time * 0.2);
+
+        vec2 r = vec2(0.);
+        r.x = fbm( uv2 + 1.0*q + vec2(1.7,0.2)+ 0.1*u_time );
+        r.y = fbm( uv2 + 1.0*q + vec2(0.3,0.8)+ 0.1*u_time);
+
+        float f = fbm(uv2+r * fbm(uv2 + r * fbm(uv2 + r)));
+        vec3 c = vec3(0.);
+        c = mix(skycolour1, skycolour2, clamp((f*f*f + f * 0.85),0.0,1.0));
+        color += c;
+
+        float numLabel = label(vUv);
+        color += numLabel;
+        gl_FragColor = vec4(color, 1.);
+    }
+`
+
+const vertexShader = glsl`
+varying vec2 vUv;
+
+void main()
+{
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+}`
+
+import { Vector2, ShaderMaterial } from 'three'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import numbers from '../numLabels/numbers.js'
+import preload from '../preload/preload.js'
+import usefulFunctions from '../usefulFunctions/usefulFunctions.js'
+
+// const { nodes } = useGLTF('./Models/tv3.glb')
+// console.log(nodes)
+
+const material = new ShaderMaterial({
+    vertexShader: vertexShader,
+
+    //use for shaders <425
+    //fragmentShader: fragment
+
+    //use for shader >= 425
+    //clean up the fragment shader
+    //imports from preload, numbers and useful functions
+    fragmentShader: preload + usefulFunctions + numbers + fragmentShader,
+    uniforms: {
+        u_time: { type: "f", value: 1.0 },
+        u_resolution: { type: "v2", value: new Vector2() },
+        u_mouse: { type: "v2", value: new Vector2() }
+    }
+})
+
+// console.log(material.fragmentShader)
+
+export default function Shader595()
+{
+    const meshRef = useRef()
+    
+    useFrame(({clock}) => {
+        meshRef.current.material.uniforms.u_time.value = clock.elapsedTime
+    })
+
+    return (
+        <>
+            <mesh ref={meshRef} material={material}>
+                <planeGeometry args={[2, 2, 1, 1]} />
+            </mesh>
+        </>
+    )
+}
