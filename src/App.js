a8fd3d9259b07ebb8d6668cb2144eb48381fe534
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-09-26 10:12:57 -0400

    shader no. 623

--- src/App.js
+++ src/App.js
@@ -623,6 +623,7 @@ import Shader619 from './shaders/619/fragment.js'
 import Shader620 from './shaders/620/fragment.js'
 import Shader621 from './shaders/621/fragment.js'
 import Shader622 from './shaders/622/fragment.js'
+import Shader623 from './shaders/623/fragment.js'
 
 export default function App()
 {
@@ -1251,7 +1252,8 @@ export default function App()
         <Shader619 />,
         <Shader620 />,
         <Shader621 />,
-        <Shader622 />
+        <Shader622 />,
+        <Shader623 />
     ]
 
     return (
--- src/UI/Interface.js
+++ src/UI/Interface.js
@@ -11,7 +11,7 @@ export default function Interface()
     const infoActive = useShader(state => state.activateInformation)
     const infoDeactivate = useShader(state => state.deactivateInformation)
 
-    const total = 622
+    const total = 623
     // console.log(total)
     
     const goNext = () => {
