aabd556223acf707e2070dda3b0819d7e44b1bc3
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-12-19 09:38:29 -0500

    shader 679

--- src/App.js
+++ src/App.js
@@ -680,6 +680,7 @@ import Shader675 from './shaders/675/fragment.js'
 import Shader676 from './shaders/676/fragment.js'
 import Shader677 from './shaders/677/fragment.js'
 import Shader678 from './shaders/678/fragment.js'
+import Shader679 from './shaders/679/fragment.js'
 
 export default function App()
 {
@@ -1364,7 +1365,8 @@ export default function App()
         <Shader675 />,
         <Shader676 />,
         <Shader677 />,
-        <Shader678 />
+        <Shader678 />,
+        <Shader679 />
     ]
 
     return (
