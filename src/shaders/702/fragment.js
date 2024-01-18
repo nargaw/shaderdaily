cb94b665b2c3611c1928e0f7248b33f7934257d6
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-01-18 10:09:13 -0500

    shader 702

--- src/shaders/702/fragment.js
+++ src/shaders/702/fragment.js
@@ -5,8 +5,8 @@ const fragmentShader = glsl`
     #define S(a, b, t) smoothstep(a, b, t)
     #define time u_time
 
-    uniform sampler2D u_texture;
-    uniform samplerCube u_background;
+    uniform sampler2D u_diffuse1;
+    uniform sampler2D u_diffuse2;
 
     float inverseLerp(float v, float minValue, float maxValue) {
     return (v - minValue) / (maxValue - minValue);
@@ -17,7 +17,6 @@ const fragmentShader = glsl`
     return mix(outMin, outMax, t);
     }
       
-
     float label(vec2 p)
     {
 
@@ -32,113 +31,96 @@ const fragmentShader = glsl`
     }
 
     // The MIT License
-// Copyright © 2013 Inigo Quilez
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// https://www.youtube.com/c/InigoQuilez
-// https://iquilezles.org/
-//
-// https://www.shadertoy.com/view/Xsl3Dl
-vec3 hash( vec3 p ) // replace this by something better
-{
-	p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
-            dot(p,vec3(269.5,183.3,246.1)),
-            dot(p,vec3(113.5,271.9,124.6)));
+    // Copyright © 2013 Inigo Quilez
+    // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    // https://www.youtube.com/c/InigoQuilez
+    // https://iquilezles.org/
+    //
+    // https://www.shadertoy.com/view/Xsl3Dl
+    vec3 hash( vec3 p ) // replace this by something better
+    {
+        p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
+                dot(p,vec3(269.5,183.3,246.1)),
+                dot(p,vec3(113.5,271.9,124.6)));
 
-	return -1.0 + 2.0*fract(sin(p)*43758.5453123);
-}
-
-float noise( in vec3 p )
-{
-  vec3 i = floor( p );
-  vec3 f = fract( p );
-	
-	vec3 u = f*f*(3.0-2.0*f);
-
-  return mix( mix( mix( dot( hash( i + vec3(0.0,0.0,0.0) ), f - vec3(0.0,0.0,0.0) ), 
-                        dot( hash( i + vec3(1.0,0.0,0.0) ), f - vec3(1.0,0.0,0.0) ), u.x),
-                   mix( dot( hash( i + vec3(0.0,1.0,0.0) ), f - vec3(0.0,1.0,0.0) ), 
-                        dot( hash( i + vec3(1.0,1.0,0.0) ), f - vec3(1.0,1.0,0.0) ), u.x), u.y),
-              mix( mix( dot( hash( i + vec3(0.0,0.0,1.0) ), f - vec3(0.0,0.0,1.0) ), 
-                        dot( hash( i + vec3(1.0,0.0,1.0) ), f - vec3(1.0,0.0,1.0) ), u.x),
-                   mix( dot( hash( i + vec3(0.0,1.0,1.0) ), f - vec3(0.0,1.0,1.0) ), 
-                        dot( hash( i + vec3(1.0,1.0,1.0) ), f - vec3(1.0,1.0,1.0) ), u.x), u.y), u.z );
-}
-
-float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
-    float amplitude = 1.0;
-    float frequency = 1.0;
-    float total = 0.0;
-    float normalization = 0.0;
-  
-    for (int i = 0; i < octaves; ++i) {
-      float noiseValue = noise(p * frequency);
-      total += noiseValue * amplitude;
-      normalization += amplitude;
-      amplitude *= persistence;
-      frequency *= lacunarity;
+        return -1.0 + 2.0*fract(sin(p)*43758.5453123);
     }
-  
-    total /= normalization;
-    total = smoothstep(-1.0, 1.0, total);
-  
-    return total;
-  }
 
-  vec3 GenerateSky(){
-    vec3 color1 = vec3(0.4, 0.6, 0.9);
-    vec3 color2 = vec3(0.1, 0.15, 0.4);
-    return mix(color1, color2, smoothstep(0., 1., vUv.y));
-  }
-  
-  vec3 DrawMountains(vec3 background, vec3 mountainColor, vec2 pixelCoords, float depth){
+    float noise( in vec3 p )
+    {
+    vec3 i = floor( p );
+    vec3 f = fract( p );
+        
+        vec3 u = f*f*(3.0-2.0*f);
 
-    // float y = sin(pixelCoords.x/256.) * 256.0;
-    float y = fbm(vec3(depth + pixelCoords.x/256.0, 1.432, 3.643), 6, 0.5, 2.0) * 256.0;
+    return mix( mix( mix( dot( hash( i + vec3(0.0,0.0,0.0) ), f - vec3(0.0,0.0,0.0) ), 
+                            dot( hash( i + vec3(1.0,0.0,0.0) ), f - vec3(1.0,0.0,0.0) ), u.x),
+                    mix( dot( hash( i + vec3(0.0,1.0,0.0) ), f - vec3(0.0,1.0,0.0) ), 
+                            dot( hash( i + vec3(1.0,1.0,0.0) ), f - vec3(1.0,1.0,0.0) ), u.x), u.y),
+                mix( mix( dot( hash( i + vec3(0.0,0.0,1.0) ), f - vec3(0.0,0.0,1.0) ), 
+                            dot( hash( i + vec3(1.0,0.0,1.0) ), f - vec3(1.0,0.0,1.0) ), u.x),
+                    mix( dot( hash( i + vec3(0.0,1.0,1.0) ), f - vec3(0.0,1.0,1.0) ), 
+                            dot( hash( i + vec3(1.0,1.0,1.0) ), f - vec3(1.0,1.0,1.0) ), u.x), u.y), u.z );
+    }
 
+    float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
+        float amplitude = 1.0;
+        float frequency = 1.0;
+        float total = 0.0;
+        float normalization = 0.0;
+    
+        for (int i = 0; i < octaves; ++i) {
+        float noiseValue = noise(p * frequency);
+        total += noiseValue * amplitude;
+        normalization += amplitude;
+        amplitude *= persistence;
+        frequency *= lacunarity;
+        }
+    
+        total /= normalization;
+        total = smoothstep(-1.0, 1.0, total);
+    
+        return total;
+    }
 
-    vec3 fogColor = vec3(0.4, 0.6, 0.9);
-    float fogFactor = smoothstep(0., 8000., depth) * 0.5;
-
-    float heightFactor = smoothstep(256., -512.0, pixelCoords.y);
-    heightFactor *= heightFactor;
-    fogFactor = mix(heightFactor, fogFactor, fogFactor);
-
-    mountainColor = mix(mountainColor, fogColor, fogFactor );
-
-    float sdfMountain = pixelCoords.y - y;
-    float blur = 1.0 + smoothstep(200., 6000., depth) * 128. + smoothstep(200., -1400., depth) * 128.;
-    vec3 color = mix(mountainColor, background, smoothstep(0., blur, sdfMountain));
-    return color;
-  }
-
+    float sdfCircle(vec2 p, float r){
+        return length(p) - r;
+    }
 
     void main()
     {
         vec2 pixelCoords = (vUv - 0.5) * u_resolution;
-        vec3 color = GenerateSky();
+        vec3 color;
+        float noiseSample = fbm(vec3(pixelCoords, 0.) * 0.005, 4, 0.5, 2.0);
+        float size = smoothstep(0., 15., mod(u_time, 15.)) * ( 50. + length(u_resolution) * 0.5);
+        float d = sdfCircle(pixelCoords + 50. * noiseSample, size);
+
         
-        vec2 timeOffset = vec2(time * 50., 0.);
-        vec2 mountainCoords = (pixelCoords - vec2(0., 400.)) * 8. + timeOffset ;
-        color = DrawMountains(color, vec3(0.08, 0.41, 0.74), mountainCoords, 6000.);
+        vec2 distortion = noiseSample / u_resolution;
 
-        mountainCoords = (pixelCoords - vec2(0., 360.)) * 4. + timeOffset;
-        color = DrawMountains(color, vec3(0.14, 0.40, 0.64), mountainCoords, 3200.);
+        vec2 uvDistortion = distortion * 20. * smoothstep(80., 20., d);
+        vec3 sample1 = texture2D(u_diffuse1, vUv + uvDistortion).xyz;
+        vec3 sample2 = texture2D(u_diffuse2, vUv).xyz;
+        color = sample1;
 
-        mountainCoords = (pixelCoords - vec2(0., 280.)) * 2. + timeOffset;
-        color = DrawMountains(color, vec3(0.21, 0.39, 0.55), mountainCoords, 1600.);
+        
 
-        mountainCoords = (pixelCoords - vec2(0., 150.)) * 1. + timeOffset;
-        color = DrawMountains(color, vec3(0.29, 0.38, 0.45), mountainCoords, 800.);
+        float burnAmount = 1.0 - exp(-d * d * 0.001);
+        color = mix(vec3(0.), color, burnAmount);
 
-        mountainCoords = (pixelCoords - vec2(0., -100.)) * 0.5 + timeOffset;
-        color = DrawMountains(color, vec3(0.38, 0.37, 0.36), mountainCoords, 400.);
+        vec3 fireColor = vec3(1., 0.5, 0.2);
+        float orangeAmount = smoothstep(0., 10., d);
+        orangeAmount = pow(orangeAmount, 0.25);
+        color = mix(fireColor, color, orangeAmount);
 
-        mountainCoords = (pixelCoords - vec2(0., -500.)) * 0.25 + timeOffset;
-        color = DrawMountains(color, vec3(0.46, 0.36, 0.26), mountainCoords, 200.);
+        color = mix(sample2, color, smoothstep(0., 1., d));
 
-        mountainCoords = (pixelCoords - vec2(0., -1400.)) * 0.125 + timeOffset;
-        color = DrawMountains(color, vec3(0.55, 0.35, 0.17), mountainCoords, 0.);
-    
+        //firey glow
+        float glowAmount = smoothstep(0., 32., abs(d));
+        glowAmount = 1. - pow(glowAmount, 0.125);
+        color += glowAmount * vec3(1., 0.2, 0.05);
+
+        
 
         float numLabel = label(vUv);
         color = mix(color, vec3(1.), numLabel) ;
@@ -182,12 +164,9 @@ export default function Shader702()
 
     const loader = new THREE.TextureLoader()
 
-    const tvTexture = loader.load('./Models/Textures/TV/tv.jpg')
-    const colorsTexture = loader.load('./Models/Textures/Colors/texture.png')
-    colorsTexture.minFilter = THREE.NearestFilter
-    colorsTexture.magFilter = THREE.NearestFilter
-    const suz = useGLTF('./Models/suzanne/suz.glb')
-    const testMaterial = new THREE.MeshNormalMaterial()
+    const menelik = loader.load('./Models/Textures/photos/menelik.jpg')
+    const ras = loader.load('./Models/Textures/photos/ras.jpg')
+    console.log(menelik, ras)
     
     const material = new THREE.ShaderMaterial({
         vertexShader: vertexShader ,
@@ -197,8 +176,8 @@ export default function Shader702()
             u_time: { type: "f", value: 1.0 },
             u_resolution: { type: "v2", value: new Vector2(window.innerWidth, window.innerHeight) },
             u_mouse: { type: "v2", value: new Vector2() },
-            u_texture: {value: colorsTexture},
-            u_background: {value: textureCube}
+            u_diffuse1: {value: menelik},
+            u_diffuse2: {value: ras}
         },
     })
 
