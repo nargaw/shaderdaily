a2a3ea1e90ac0703f8021003c3e0f78246350513
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-02-22 06:14:34 -0500

    shader no. 715

--- src/shaders/715/fragment.js
+++ src/shaders/715/fragment.js
@@ -6,8 +6,8 @@ const fragmentShader = glsl`
     #define time u_time
 
     uniform sampler2D u_texture;
-    uniform float u_vignette1;
-    uniform float u_vignette2;
+    uniform float u_waveFactor1;
+    uniform float u_waveFactor2;
     // uniform float u_factor2;
     // uniform float u_speed;
 
@@ -45,58 +45,10 @@ const fragmentShader = glsl`
     void main()
     {
         vec2 coords = vUv;
-        // vec2 coords = fract(vUv * vec2(2., 1.));
-        // coords.x = remap(coords.x, 0., 1., 0.25, 0.75);
         vec3 color;
        
         color = texture2D(u_texture, coords).rgb;
-
-            //tinting
-            // vec3 tintColor = vec3(1., 0.5, 0.5);
-            // color *= tintColor;
-
-            //brightness
-            // float brightnessAmount = 0.1;
-            // color += brightnessAmount;
-
-            //saturation
-            float luminance = dot(color, vec3(0.2126, 0.7152, 0.0722));
-            float saturationAmount = 2.;
-            // color = mix(vec3(luminance), color, saturationAmount);
-
-            //contrast
-            // float contrastAmount = 2.;
-            // float midpoint = 0.5;
-            // color = saturate3((color - midpoint) * contrastAmount + midpoint);
-            // vec3 sg = sign(color - midpoint);
-            // color = sg * pow(abs(color - midpoint) * 2.0, vec3(1./contrastAmount)) * 0.5 + midpoint;
-
-
-            //matrixeffect
-            // color = pow(color, vec3(1.5, 0.8, 1.5));
-
-            //colorBoost
-            // vec3 refColor = vec3(0.72, 0.25, 0.25);
-            // float colorWeight = 1.0 - distance(color, refColor);
-            // colorWeight = smoothstep(0.5, 1., colorWeight);
-            // color = mix(vec3(luminance), color, colorWeight);
-
-            //vector color boost
-            // vec3 refColor = vec3(0.72, 0.25, 0.25);
-            // float colorWeight = dot(normalize(color), normalize(refColor));
-            // colorWeight = pow(colorWeight, 32.);
-            // color = mix(vec3(luminance), color, colorWeight);
-
-            //vignette
-            vec2 vignetteCoords = fract(vUv * vec2(2., 1.));
-            float v1 = smoothstep(u_vignette1, u_vignette2, abs(vignetteCoords.x - 0.5));
-            float v2 = smoothstep(u_vignette1, u_vignette2, abs(vignetteCoords.y - 0.5));
-            float vignetteAmount = v1 * v2;
-            vignetteAmount = pow(vignetteAmount, 0.5);
-            vignetteAmount = remap(vignetteAmount, 0., 1., 0.5, 1.);
-            color *= vignetteAmount;
        
-
         float numLabel = label(vUv);
         color = mix(color, vec3(1.), numLabel) ;
         gl_FragColor = vec4(color, 1.);
@@ -106,89 +58,16 @@ const fragmentShader = glsl`
 const vertexShader = glsl`
 varying vec2 vUv;
 uniform float u_time;
-
-//	Classic Perlin 3D Noise 
-//	by Stefan Gustavson
-//
-vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
-vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
-vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
-
-float cnoise(vec3 P){
-    vec3 Pi0 = floor(P); // Integer part for indexing
-    vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
-    Pi0 = mod(Pi0, 289.0);
-    Pi1 = mod(Pi1, 289.0);
-    vec3 Pf0 = fract(P); // Fractional part for interpolation
-    vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
-    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
-    vec4 iy = vec4(Pi0.yy, Pi1.yy);
-    vec4 iz0 = Pi0.zzzz;
-    vec4 iz1 = Pi1.zzzz;
-
-    vec4 ixy = permute(permute(ix) + iy);
-    vec4 ixy0 = permute(ixy + iz0);
-    vec4 ixy1 = permute(ixy + iz1);
-
-    vec4 gx0 = ixy0 / 7.0;
-    vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
-    gx0 = fract(gx0);
-    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
-    vec4 sz0 = step(gz0, vec4(0.0));
-    gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-    gy0 -= sz0 * (step(0.0, gy0) - 0.5);
-
-    vec4 gx1 = ixy1 / 7.0;
-    vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
-    gx1 = fract(gx1);
-    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-    vec4 sz1 = step(gz1, vec4(0.0));
-    gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-    gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-
-    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-
-    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-    g000 *= norm0.x;
-    g010 *= norm0.y;
-    g100 *= norm0.z;
-    g110 *= norm0.w;
-    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-    g001 *= norm1.x;
-    g011 *= norm1.y;
-    g101 *= norm1.z;
-    g111 *= norm1.w;
-
-    float n000 = dot(g000, Pf0);
-    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-    float n111 = dot(g111, Pf1);
-
-    vec3 fade_xyz = fade(Pf0);
-    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
-    return 2.2 * n_xyz;
-}
-
+uniform float u_waveFactor1;
 
 void main()
 {
     vUv = uv;
     vec3 localSpacePosition = position;
-    localSpacePosition.x += sin(u_time);
+    // localSpacePosition.x += sin(u_time);
+    float wave = sin(localSpacePosition.x * u_waveFactor1 + u_time) ;
     // gl_Position = vec4(localSpacePosition, 1.);
+    localSpacePosition.z += wave;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.);
 }`
 
@@ -219,18 +98,12 @@ export default function Shader715()
     forest.wrapS = THREE.MirroredRepeatWrapping
     forest.wrapT = THREE.MirroredRepeatWrapping
 
-    const {vignetteAmount1, vignetteAmount2} = useControls({
-        vignetteAmount1: {
-            value: 0.5,
+    const {wave1} = useControls({
+        wave1: {
+            value: 2.,
             min: 0.,
-            max: 1.,
-            step: 0.01
-        },
-        vignetteAmount2: {
-            value: 0.25,
-            min: 0.,
-            max: 1.,
-            step: 0.01
+            max: 5.,
+            step: 0.1
         }
     })
     
@@ -243,12 +116,11 @@ export default function Shader715()
             u_resolution: { type: "v2", value: new Vector2(window.innerWidth, window.innerHeight) },
             u_mouse: { type: "v2", value: new Vector2() },
             u_texture: {value: forest},
-            u_vignette1: {value: vignetteAmount1},
-            u_vignette2: {value: vignetteAmount2}
+            u_waveFactor1: {value: wave1},
         },
     })
 
-    const geometry = new THREE.PlaneGeometry(2., 2.)
+    const geometry = new THREE.PlaneGeometry(2., 2., 256, 256)
 
     const icosaGeometry = new THREE.IcosahedronGeometry(1, 128)
     const meshRef = useRef()
