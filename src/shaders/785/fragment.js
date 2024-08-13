5a71273b78e4202407942bdeb72885f94ab16d2e
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-08-12 22:17:15 -0400

    shader785

--- src/shaders/785/fragment.js
+++ src/shaders/785/fragment.js
@@ -5,6 +5,8 @@ const fragmentShader = glsl`
     #define S(a, b, t) smoothstep(a, b, t)
     #define time u_time
 
+    varying vec3 vPosition;
+    varying vec3 vNormal;
     uniform sampler2D u_texture;
     uniform sampler2D u_texture2;
     uniform vec2 u_mouse2;
@@ -23,64 +25,18 @@ const fragmentShader = glsl`
         return left + center + right ;
     }
 
-    //	Classic Perlin 2D Noise 
-    //	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
-    //
-    vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
-
-    float cnoise(vec2 P){
-    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
-    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
-    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
-    vec4 ix = Pi.xzxz;
-    vec4 iy = Pi.yyww;
-    vec4 fx = Pf.xzxz;
-    vec4 fy = Pf.yyww;
-    vec4 i = permute(permute(ix) + iy);
-    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
-    vec4 gy = abs(gx) - 0.5;
-    vec4 tx = floor(gx + 0.5);
-    gx = gx - tx;
-    vec2 g00 = vec2(gx.x,gy.x);
-    vec2 g10 = vec2(gx.y,gy.y);
-    vec2 g01 = vec2(gx.z,gy.z);
-    vec2 g11 = vec2(gx.w,gy.w);
-    vec4 norm = 1.79284291400159 - 0.85373472095314 * 
-        vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
-    g00 *= norm.x;
-    g01 *= norm.y;
-    g10 *= norm.z;
-    g11 *= norm.w;
-    float n00 = dot(g00, vec2(fx.x, fy.x));
-    float n10 = dot(g10, vec2(fx.y, fy.y));
-    float n01 = dot(g01, vec2(fx.z, fy.z));
-    float n11 = dot(g11, vec2(fx.w, fy.w));
-    vec2 fade_xy = fade(Pf.xy);
-    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
-    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
-    return 2.3 * n_xy;
-    }
    
     void main()
     {
         vec2 coords = vUv;
-
-        float noise = cnoise(coords) * 0.15;
-        vec2 displaceCoords = coords + noise - u_mouse + 0.5;
-        // displaceCoords = Rot(displaceCoords, u_time);
-
-        
+        vec3 normal = normalize(vNormal);
         vec3 color = vec3(0.);
         vec2 m = u_mouse.xy;
         vec2 offset  =  vec2(u_mouse.xy);  
         vec2 offset2 = vec2(u_mouse2.xy); 
         vec2 offset3 = vec2(u_mouse3.xy);
 
-        vec4 displacement = texture2D(u_texture2, displaceCoords);
-        vec2 dir = -u_mouse + 0.5;
-        vec2 uv = coords + dir * displacement.a * 0.25;
-        color = texture2D(u_texture, uv).rgb;
-
+        color = normal * 0.5 + 0.5;
         float numLabel = label(vUv);
         color = mix(color, vec3(1.), numLabel) ;
         gl_FragColor = vec4(color, 1.);
