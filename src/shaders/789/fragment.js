dd7f648614207ebec64449568ab7a1b562d238fa
Nate Argaw <nateargaw@gmail.com>
2024-08-24 22:01:55 -0400

    shader789

--- src/shaders/789/fragment.js
+++ src/shaders/789/fragment.js
@@ -64,7 +64,7 @@ const fragmentShader = glsl`
 
         float numLabel = label(vUv);
         // color = mix(color, vec3(1.), numLabel) ;
-        gl_FragColor = vec4(normalize(vNormal), 1.);
+        gl_FragColor = vec4((vNormal), 1.);
         #include <tonemapping_fragment>
         #include <colorspace_fragment>
     }
