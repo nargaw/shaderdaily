7477c9d1b7a31988f7bafff94aa5ff0846e24cfa
Nate A <59675817+nargaw@users.noreply.github.com>
2024-12-31 07:17:05 -0500

    shader 850

--- src/App.js
+++ src/App.js
@@ -851,6 +851,7 @@ import Shader846 from './shaders/846/fragment.js'
 import Shader847 from './shaders/847/fragment.js'
 import Shader848 from './shaders/848/fragment.js'
 import Shader849 from './shaders/849/fragment.js'
+import Shader850 from './shaders/850/fragment.js'
 
 export default function App()
 {
@@ -1706,7 +1707,8 @@ export default function App()
         <Shader846 />,
         <Shader847 />,
         <Shader848 />,
-        <Shader849 />
+        <Shader849 />,
+        <Shader850 />
     ]
 
     return (
--- src/UI/Interface.js
+++ src/UI/Interface.js
@@ -14,7 +14,7 @@ export default function Interface()
     const infoActive = useShader(state => state.activateInformation)
     const infoDeactivate = useShader(state => state.deactivateInformation)
 
-    const total = 849
+    const total = 850
     // console.log(total)
     
     const goNext = () => {
