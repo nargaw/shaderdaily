2c1a7aca2dda01e64a0e1ee3cf821ea2acf59a7f
Nate <nateargaw@gmail.com>
2025-05-05 05:34:05 -0400

    update aspect ratio to be automatic

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ root.render(
                 hideCopyButton = {true}
             />
         </div>
-        <Canvas className="webgl" camera={{fov: 50, aspect: window.innerWidth/window.innerHeight, near: 0.01, far: 1000, position: [0, 0, 5]}}>
+        <Canvas className="webgl" camera={{fov: 50, near: 0.01, far: 1000, position: [0, 0, 5]}}>
             <App />
             {/* <OrbitControls /> */}
             <Model />
