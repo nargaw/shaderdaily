8432c50ca963ae6fcc61a227cffac021147a9f38
Nate <nateargaw@gmail.com>
2025-12-08 06:05:15 -0500

    chore(interface.js): update total count

--- src/UI/Interface.js
+++ src/UI/Interface.js
@@ -14,7 +14,7 @@ export default function Interface()
     const infoActive = useShader(state => state.activateInformation)
     const infoDeactivate = useShader(state => state.deactivateInformation)
 
-    const total = 984
+    const total = 985
     // console.log(total)
     
     const goNext = () => {
