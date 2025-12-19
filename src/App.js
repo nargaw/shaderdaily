1550d17b3b0b60d9dbd4cba92f43bd88f05fa99a
Nate <nateargaw@gmail.com>
2025-12-19 05:47:00 -0500

    chore(App.js): add shader 992

--- src/App.js
+++ src/App.js
@@ -993,6 +993,7 @@ import Shader988 from './shaders/988/fragment.js'
 import Shader989 from './shaders/989/fragment.js'
 import Shader990 from './shaders/990/fragment.js'
 import Shader991 from './shaders/991/fragment.js'
+import Shader992 from './shaders/992/fragment.js'
 
 export default function App()
 {
@@ -1990,7 +1991,8 @@ export default function App()
         <Shader988 />,
         <Shader989 />,
         <Shader990 />,
-        <Shader991 />
+        <Shader991 />,
+        <Shader992 />
     ]
 
     return (
